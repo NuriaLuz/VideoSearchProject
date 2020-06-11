6a4dda1ecc79b70c24c073f70d47d1ffaab883fb
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-06-11 16:55:23 -0400

    videos showing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import VideoList from './VideoList'
 import youtube from '../Api/Youtube'
 const KEY = 'AIzaSyAJAvGoQbnpy015psAN5-RGu9lhZRQlTpk'
 
@@ -25,7 +26,8 @@ class App extends React.Component{
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTextSubmit}/>
-                I have {this.state.videos.length} videos.
+
+                <VideoList videos={this.state.videos}/> 
             </div>
         )
     }
---
+++ src/components/VideoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const VideoItem = ({video}) => {
+    return(
+        <div>
+            {video.snippet.title}
+            <img src = {video.snippet.thumbnails.medium.url} />
+        </div>
+    )
+}
+
+export default VideoItem
